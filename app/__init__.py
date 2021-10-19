edfcc09b8d3b89c1c184c28af7808876a85e1898
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-19 14:40:12 -0700

    Create author, get books by author, post book by author

--- app/__init__.py
+++ app/__init__.py
@@ -14,12 +14,14 @@ def create_app(test_config=None):
 
     # Import models here
     from app.models.book import Book
+    from app.models.author import Author
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    from .routes import books_bp
+    from .routes import books_bp, authors_bp
     app.register_blueprint(books_bp)
+    app.register_blueprint(authors_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import request, Blueprint, make_response, jsonify
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
@@ -53,3 +54,44 @@ def handle_book(book_id):
         db.session.delete(book)
         db.session.commit()
         return make_response(f"Book #{book.id} successfully deleted")
+
+@authors_bp.route("", methods=["POST"])
+def create_author():
+        request_body = request.get_json()
+        new_author = Author(name=request_body["name"])
+
+        db.session.add(new_author)
+        db.session.commit()
+
+        return make_response(f"Author #{new_author.id} successfully created", 201)
+
+
+@authors_bp.route("/<author_id>/books", methods=["GET", "POST"])
+def handle_author_books(author_id):
+
+    author = Author.query.get(author_id)
+    if author is None:
+        return "Author not found", 404
+
+    if request.method == "GET":
+        books_response = []
+        for book in author.books:
+            books_response.append(
+                {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+                }
+            )
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"],
+                        author_id=author_id)
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} by {new_book.author.name} successfully created", 201)
+

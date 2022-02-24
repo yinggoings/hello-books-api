0d2e4d2c1aadbdd9d0c9c9b5096659b5aa6a1cea
Ying G <yingg@yings-air.lan>
2022-02-24 12:27:42 -0800

    extend blueprint for viewing individual books

--- app/routes.py
+++ app/routes.py
@@ -12,9 +12,9 @@ books = [
     Book(3,"Harry Potter","fiction")
     ]
 
-books_bp = Blueprint("books_bp",__name__)
+books_bp = Blueprint("books_bp",__name__,url_prefix="/books")
 
-@books_bp.route("/books",methods=["GET"])
+@books_bp.route("",methods=["GET"])
 def books_info():
     books_response = []
     for book in books:
@@ -24,3 +24,16 @@ def books_info():
             "description":book.description}
         )
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>",methods=["GET"])
+def book_info(book_id):
+    if book_id.isdigit():
+        book_id = int(book_id)
+        for book in books:
+            if book.id == book_id:
+                return {
+                    "id":book.id,
+                    "title":book.title,
+                    "description":book.description
+                }
+    abort(404)

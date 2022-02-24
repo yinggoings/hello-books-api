fc734f17108d624bfc6c9d99ef2242fd7aca2745
Ying G <yingg@yings-air.lan>
2022-02-24 12:26:15 -0800

    set up and link blueprint for viewing all books

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort
 
+class Book:
+    def __init__(self,id,title,description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1,"The Watchmen","comic"),
+    Book(2,"The New York Times","newspaper"),
+    Book(3,"Harry Potter","fiction")
+    ]
+
+books_bp = Blueprint("books_bp",__name__)
+
+@books_bp.route("/books",methods=["GET"])
+def books_info():
+    books_response = []
+    for book in books:
+        books_response.append(
+            {"id":book.id,
+            "title":book.title,
+            "description":book.description}
+        )
+    return jsonify(books_response)

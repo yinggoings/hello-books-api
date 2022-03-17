16c998b28a064c7a91ab5c4ef28e161d584d7f2c
Ying G <yingg@yings-air.lan>
2022-03-17 15:40:35 -0700

    fix var naming errors

--- app/models/book.py
+++ app/models/book.py
@@ -5,4 +5,4 @@ class Book(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     author_id = db.Column(db.Integer,db.ForeignKey('author.id'))
-    authors = db.relationship("Author",back_populates="books")
+    author = db.relationship("Author",back_populates="books")

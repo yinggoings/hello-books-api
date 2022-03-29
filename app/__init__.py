6915af10d17236935b354b877ab93596a6e704e1
Ying G <yingg@yings-air.lan>
2022-03-28 21:08:48 -0700

    add book_genre model and define many-to-many relationship between book and genre models

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,7 @@ def create_app(test_config=None):
     from app.models.book import Book
     from app.models.author import Author
     from app.models.genre import Genre
+    from app.models.book_genre import BookGenre
 
     from .routes import books_bp, authors_bp, genres_bp
     app.register_blueprint(books_bp)
--- app/models/book_genre.py
+++ app/models/book_genre.py
@@ -2,4 +2,4 @@ from app import db
 
 class BookGenre(db.Model):
     book_id = db.Column(db.Integer,db.ForeignKey('book.id'),primary_key=True,nullable=False)
-    genre_id = db.Column(db.String,db.ForeignKey('genre.id'),primary_key=True,nullable=False)
+    genre_id = db.Column(db.Integer,db.ForeignKey('genre.id'),primary_key=True,nullable=False)

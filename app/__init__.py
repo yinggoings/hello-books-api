4b3a11a8f786ea13d07bb7d12349965e25cc0d2b
Ying G <yingg@yings-air.lan>
2022-03-28 19:58:42 -0700

    register genres BP in models __init__

--- app/__init__.py
+++ app/__init__.py
@@ -24,9 +24,11 @@ def create_app(test_config=None):
 
     from app.models.book import Book
     from app.models.author import Author
+    from app.models.genre import Genre
 
-    from .routes import books_bp, authors_bp
+    from .routes import books_bp, authors_bp, genres_bp
     app.register_blueprint(books_bp)
     app.register_blueprint(authors_bp)
+    app.register_blueprint(genres_bp)
 
     return app
